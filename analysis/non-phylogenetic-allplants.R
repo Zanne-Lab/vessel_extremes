4af77316c2d4cfdb130a03e54f3579434cd1c18c
Will Pearse <will.pearse@gmail.com>
2017-05-07 14:59:51 -0600

    Fixed binning

--- analysis/non-phylogenetic-allplants.R
+++ analysis/non-phylogenetic-allplants.R
@@ -1,6 +1,8 @@
 # Non-phylogenetic plots and models
 # Will Pearse - 2017-04-30
 
+library(MuMIn)
+
 # Load data
 growth.form <- read.csv("../speciesTraitDataAEZ3.csv", as.is=TRUE)
 env <- read.csv("../datasets/species_summaries_all.csv", as.is=TRUE)
@@ -14,37 +16,44 @@ data$tmin.025 <- data$tmin.025/10
 data$herb <- data$woodiness!="W"
 data <- na.omit(data)
 
-# Do each separate plot
-.prop.herb <- function(x, var, data){
-    x <- data[,var] <= x
-    if(sum(x)==0)
-        return(NA)
-    return(1-sum(data$herb[x])/nrow(data))
-}
-.plot.herb <- function(var, data, ...){
-    range <- range(data[,var])
-    plot(0, xlim=range, type="n", ylim=c(0,1),axes=FALSE, ...)
-    props <- seq(range[1], range[2], length.out=200)
-    for(i in seq(2, length(props)))
-        lines(props[(i-1):i], c(.prop.herb(props[i-1],var,data),.prop.herb(props[i],var,data)), col="black")
+plot.herb <- function(var, data, by, ...){
+    bins <- with(data, seq(min(data[,var]),max(data[,var]),by=by))
+    x <- with(data, table(cut(data[,var],bins), herb))[-1,]
+    weight <- as.numeric(scale(rowSums(x)[-1]))
+    weight <- (weight - min(weight) + 1) * 2
+    x <- apply(x, 1, function(y) y[2]/sum(y))
+    plot(x ~ bins[-1:-2], type="n", axes=FALSE, ylab="Proportion herbaceous", ylim=c(0,1), lwd=weight, ...)
+    for(i in seq_along(x))
+        lines(bins[(i+2):(i+3)], x[i:(i+1)], lwd=weight[i])
     axis(1)
     axis(2)
 }
+
 pdf("../output/figures/figure_1_temp.pdf")
-.plot.herb("tmin.025", data, xlab=expression(paste("Minimum temperature (",degree,")")), ylab="Proportion herbaceous")
+plot.herb("tmin.025", data, by=5, xlab=expression(paste("Minimum temperature (",degree,")")))
 dev.off()
 pdf("../output/figures/figure_1_precip.pdf")
-.plot.herb("pmin.025", data, xlab="Minimum precipitation (mm)", ylab="Proportion herbaceous")
+plot.herb("pmin.025", data, by=20, xlim=c(0,250), xlab="Minimum precipitation (mm)")
 dev.off()
 pdf("../output/figures/figure_1_temp_seas.pdf")
-.plot.herb("tseas.975", data, xlab="Temperature seasonality", ylab="Proportion herbaceous")
+plot.herb("tseas.975", data, by=500, xlim=c(0,15000), xlab="Temperature seasonality")
 dev.off()
 pdf("../output/figures/figure_1_precip_seas.pdf")
-.plot.herb("pseas.975", data, xlab="Temperature seasonality", ylab="Proportion herbaceous")
+plot.herb("pseas.975", data, by=20, xlim=c(0,200), xlab="Temperature seasonality")
 dev.off()
 pdf("../output/figures/figure_1_lat_lower.pdf")
-.plot.herb("decimallatitude.025", data, xlab=expression(paste("Minimum Southern latitude (",degree,")")), ylab="Proportion herbaceous")
+plot.herb("decimallatitude.025", data, by=5, xlim=c(-60,0), xlab=expression(paste("Minimum Southern latitude (",degree,")")))
 dev.off()
 pdf("../output/figures/figure_1_lat_upper.pdf")
-.plot.herb("decimallatitude.975", data, xlab=expression(paste("Maximum Northern latitude (",degree,")")), ylab="Proportion herbaceous")
+plot.herb("decimallatitude.975", data, by=5, xlim=c(0,90), xlab=expression(paste("Maximum Northern latitude (",degree,")")))
 dev.off()
+
+# Let's do some modelling (?)
+
+model <- glm(herb ~ tmin.025 + I(log10(pmin.025+1)) + pseas.975 + tseas.975, data=data, na.action="na.pass")
+model.set <- dredge(model)
+model.avg <- model.avg(model.set, subset=delta<4)
+summary(model.avg)
+
+with(data, boxplot(log10(pmin.025+1) ~ herb))
+with(data, boxplot(tmin.025 ~ herb))
